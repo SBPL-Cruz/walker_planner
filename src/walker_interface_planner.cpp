4a8694c67a65f6bc81ae93224dd532326be848b5
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-07-11 13:22:32 -0400

    Moved planner config related functions and structs to a header.

--- src/walker_interface_planner.cpp
+++ src/walker_interface_planner.cpp
@@ -33,26 +33,19 @@
 #include <stdlib.h>
 #include <string>
 #include <thread>
-#include <vector>
 #include <ros/package.h>
 
 // system includes
-#include <eigen_conversions/eigen_msg.h>
 #include <leatherman/print.h>
 #include <leatherman/utils.h>
-#include <moveit_msgs/GetMotionPlan.h>
-#include <moveit_msgs/PlanningScene.h>
-#include <ros/ros.h>
 #include <kdl_conversions/kdl_msg.h>
 #include <smpl/ros/planner_interface.h>
 #include <smpl/heuristic/euclid_fullbody_heuristic.h>
 #include <smpl/heuristic/bfs_fullbody_heuristic.h>
-#include <smpl/spatial.h>
 #include <smpl/distance_map/edge_euclid_distance_map.h>
 #include <smpl/distance_map/euclid_distance_map.h>
 #include <smpl/ros/propagation_distance_field.h>
 #include <sbpl_collision_checking/collision_space.h>
-#include <sbpl_kdl_robot_model/kdl_robot_model.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <smpl/angles.h>
 #include <smpl/debug/visualizer_ros.h>
@@ -62,6 +55,8 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include <walker_planner/Path1.h>
 #include "publish_path.h"
+
+#include "planner_config.h"
 
 
 int plan(ros::NodeHandle nh, ros::NodeHandle ph, geometry_msgs::Pose grasp, octomap_msgs::OctomapWithPose octomap);
@@ -215,226 +210,6 @@ auto GetCollisionObjects(
     }
 
     return GetCollisionCubes(objects, object_ids, frame_id);
-}
-
-bool ReadInitialConfiguration(
-    ros::NodeHandle& nh,
-    moveit_msgs::RobotState& state)
-{
-    XmlRpc::XmlRpcValue xlist;
-
-    // joint_state
-    if (nh.hasParam("initial_configuration/joint_state")) {
-        ros::param::get("initial_configuration/joint_state", xlist);
-
-        if (xlist.getType() != XmlRpc::XmlRpcValue::TypeArray) {
-            ROS_WARN("initial_configuration/joint_state is not an array.");
-        }
-
-        if (xlist.size() > 0) {
-            for (int i = 0; i < xlist.size(); ++i) {
-                state.joint_state.name.push_back(std::string(xlist[i]["name"]));
-
-                if (xlist[i]["position"].getType() == XmlRpc::XmlRpcValue::TypeDouble) {
-                    state.joint_state.position.push_back(double(xlist[i]["position"]));
-                }
-                else {
-                    ROS_DEBUG("Doubles in the yaml file have to contain decimal points. (Convert '0' to '0.0')");
-                    if (xlist[i]["position"].getType() == XmlRpc::XmlRpcValue::TypeInt) {
-                        int pos = xlist[i]["position"];
-                        state.joint_state.position.push_back(double(pos));
-                    }
-                }
-            }
-        }
-    }
-    else {
-        ROS_WARN("initial_configuration/joint_state is not on the param server.");
-    }
-
-    // multi_dof_joint_state
-    if (nh.hasParam("initial_configuration/multi_dof_joint_state")) {
-        nh.getParam("initial_configuration/multi_dof_joint_state", xlist);
-
-        if (xlist.getType() == XmlRpc::XmlRpcValue::TypeArray) {
-            if (xlist.size() != 0) {
-                auto &multi_dof_joint_state = state.multi_dof_joint_state;
-                multi_dof_joint_state.joint_names.resize(xlist.size());
-                multi_dof_joint_state.transforms.resize(xlist.size());
-                for (int i = 0; i < xlist.size(); ++i) {
-                    multi_dof_joint_state.joint_names[i] = std::string(xlist[i]["joint_name"]);
-
-                    Eigen::Quaterniond q;
-                    smpl::angles::from_euler_zyx(
-                            (double)xlist[i]["yaw"], (double)xlist[i]["pitch"], (double)xlist[i]["roll"], q);
-
-                    geometry_msgs::Quaternion orientation;
-                    tf::quaternionEigenToMsg(q, orientation);
-
-                    multi_dof_joint_state.transforms[i].translation.x = xlist[i]["x"];
-                    multi_dof_joint_state.transforms[i].translation.y = xlist[i]["y"];
-                    multi_dof_joint_state.transforms[i].translation.z = xlist[i]["z"];
-                    multi_dof_joint_state.transforms[i].rotation.w = orientation.w;
-                    multi_dof_joint_state.transforms[i].rotation.x = orientation.x;
-                    multi_dof_joint_state.transforms[i].rotation.y = orientation.y;
-                    multi_dof_joint_state.transforms[i].rotation.z = orientation.z;
-                }
-            } else {
-                ROS_WARN("initial_configuration/multi_dof_joint_state array is empty");
-            }
-        } else {
-            ROS_WARN("initial_configuration/multi_dof_joint_state is not an array.");
-        }
-    }
-
-    ROS_INFO("Read initial state containing %zu joints and %zu multi-dof joints", state.joint_state.name.size(), state.multi_dof_joint_state.joint_names.size());
-    return true;
-}
-
-struct RobotModelConfig
-{
-    std::string group_name;
-    std::vector<std::string> planning_joints;
-    std::string kinematics_frame;
-    std::string chain_tip_link;
-    std::string arm_start_link;
-};
-
-bool ReadRobotModelConfig(const ros::NodeHandle &nh, RobotModelConfig &config)
-{
-    if (!nh.getParam("group_name", config.group_name)) {
-        ROS_ERROR("Failed to read 'group_name' from the param server");
-        return false;
-    }
-
-    std::string planning_joint_list;
-    if (!nh.getParam("planning_joints", planning_joint_list)) {
-        ROS_ERROR("Failed to read 'planning_joints' from the param server");
-        return false;
-    }
-
-    std::stringstream joint_name_stream(planning_joint_list);
-    while (joint_name_stream.good() && !joint_name_stream.eof()) {
-        std::string jname;
-        joint_name_stream >> jname;
-        if (jname.empty()) {
-            continue;
-        }
-        config.planning_joints.push_back(jname);
-    }
-
-    // only required for generic kdl robot model?
-    nh.getParam("kinematics_frame", config.kinematics_frame);
-    nh.getParam("chain_tip_link", config.chain_tip_link);
-    nh.getParam("arm_start_link", config.arm_start_link);
-    return true;
-}
-
-struct PlannerConfig
-{
-    std::string discretization;
-    std::string mprim_filename;
-    bool use_xyz_snap_mprim;
-    bool use_rpy_snap_mprim;
-    bool use_xyzrpy_snap_mprim;
-    bool use_short_dist_mprims;
-    double xyz_snap_dist_thresh;
-    double rpy_snap_dist_thresh;
-    double xyzrpy_snap_dist_thresh;
-    double short_dist_mprims_thresh;
-};
-
-bool ReadPlannerConfig(const ros::NodeHandle &nh, PlannerConfig &config)
-{
-    if (!nh.getParam("discretization", config.discretization)) {
-        ROS_ERROR("Failed to read 'discretization' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("mprim_filename", config.mprim_filename)) {
-        ROS_ERROR("Failed to read param 'mprim_filename' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("use_xyz_snap_mprim", config.use_xyz_snap_mprim)) {
-        ROS_ERROR("Failed to read param 'use_xyz_snap_mprim' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("use_rpy_snap_mprim", config.use_rpy_snap_mprim)) {
-        ROS_ERROR("Failed to read param 'use_rpy_snap_mprim' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("use_xyzrpy_snap_mprim", config.use_xyzrpy_snap_mprim)) {
-        ROS_ERROR("Failed to read param 'use_xyzrpy_snap_mprim' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("use_short_dist_mprims", config.use_short_dist_mprims)) {
-        ROS_ERROR("Failed to read param 'use_short_dist_mprims' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("xyz_snap_dist_thresh", config.xyz_snap_dist_thresh)) {
-        ROS_ERROR("Failed to read param 'xyz_snap_dist_thresh' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("rpy_snap_dist_thresh", config.rpy_snap_dist_thresh)) {
-        ROS_ERROR("Failed to read param 'rpy_snap_dist_thresh' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("xyzrpy_snap_dist_thresh", config.xyzrpy_snap_dist_thresh)) {
-        ROS_ERROR("Failed to read param 'xyzrpy_snap_dist_thresh' from the param server");
-        return false;
-    }
-
-    if (!nh.getParam("short_dist_mprims_thresh", config.short_dist_mprims_thresh)) {
-        ROS_ERROR("Failed to read param 'use_xyz_snap_mprim' from the param server");
-        return false;
-    }
-
-    return true;
-}
-
-auto SetupRobotModel(const std::string& urdf, const RobotModelConfig &config)
-    -> std::unique_ptr<smpl::KDLRobotModel>
-{
-    if (config.kinematics_frame.empty() || config.chain_tip_link.empty()) {
-        ROS_ERROR("Failed to retrieve param 'kinematics_frame' or 'chain_tip_link' from the param server");
-        return NULL;
-    }
-
-    ROS_INFO("Construct Generic KDL Robot Model");
-    std::unique_ptr<smpl::KDLRobotModel> rm(new smpl::KDLRobotModel);
-
-    if (!rm->init(urdf, config.kinematics_frame, config.chain_tip_link)) {
-        ROS_ERROR("Failed to initialize robot model.");
-        return NULL;
-    }
-
-    return std::move(rm);
-}
-
-auto SetupArmModel(const std::string& urdf, const RobotModelConfig &config)
-    -> std::unique_ptr<smpl::KDLRobotModel>
-{
-    if (config.arm_start_link.empty() || config.chain_tip_link.empty()) {
-        ROS_ERROR("Failed to retrieve param 'arm_start_link' or 'chain_tip_link' from the param server");
-        return NULL;
-    }
-
-    ROS_INFO("Construct Generic KDL Arm Model");
-    std::unique_ptr<smpl::KDLRobotModel> rm(new smpl::KDLRobotModel);
-
-    if (!rm->init(urdf, config.arm_start_link, config.chain_tip_link)) {
-        ROS_ERROR("Failed to initialize robot model.");
-        return NULL;
-    }
-
-    return std::move(rm);
 }
 
 class MsgSubscriber {
