db1207ea044277c6657823f90b1ef07dc4c94a7c
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-08-20 17:06:02 -0400

    Removed redundant functions from test file.

--- src/test_motion_planner.cpp
+++ src/test_motion_planner.cpp
@@ -80,78 +80,6 @@ bool constructHeuristics(
     return true;
 }
 
-template <class T>
-static auto ParseMapFromString(const std::string& s)
-    -> std::unordered_map<std::string, T>
-{
-    std::unordered_map<std::string, T> map;
-    std::istringstream ss(s);
-    std::string key;
-    T value;
-    while (ss >> key >> value) {
-        map.insert(std::make_pair(key, value));
-    }
-    return map;
-}
-
-bool IsMultiDOFJointVariable(
-    const std::string& name,
-    std::string* joint_name,
-    std::string* local_name)
-{
-    auto slash_pos = name.find_last_of('/');
-    if (slash_pos != std::string::npos) {
-        if (joint_name != NULL) {
-            *joint_name = name.substr(0, slash_pos);
-        }
-        if (local_name != NULL) {
-            *local_name = name.substr(slash_pos + 1);
-        }
-        return true;
-    } else {
-        return false;
-    }
-}
-
-std::vector<double> getResolutions(
-        smpl::RobotModel* robot,
-        const PlannerConfig& params ){
-
-    auto resolutions = std::vector<double>(robot->jointVariableCount());
-
-    std::string disc_string = params.discretization;
-    if (disc_string.empty()) {
-        ROS_ERROR("Parameter 'discretization' not found in planning params");
-    }
-
-    auto disc = ParseMapFromString<double>(disc_string);
-
-    for (size_t vidx = 0; vidx < robot->jointVariableCount(); ++vidx) {
-        auto& vname = robot->getPlanningJoints()[vidx];
-        std::string joint_name, local_name;
-        if (IsMultiDOFJointVariable(vname, &joint_name, &local_name)) {
-            // adjust variable name if a variable of a multi-dof joint
-            auto mdof_vname = joint_name + "_" + local_name;
-            auto dit = disc.find(mdof_vname);
-            if (dit == end(disc)) {
-                ROS_ERROR("Discretization for variable '%s' not found in planning parameters", vname.c_str());
-            }
-            resolutions[vidx] = dit->second;
-        } else {
-            auto dit = disc.find(vname);
-            if (dit == end(disc)) {
-                ROS_ERROR("Discretization for variable '%s' not found in planning parameters", vname.c_str());
-            }
-            resolutions[vidx] = dit->second;
-        }
-
-        ROS_ERROR("resolution(%s) = %0.3f", vname.c_str(), resolutions[vidx]);
-    }
-
-    return resolutions;
-}
-
-
 class ReadExperimentFromFile {
     public:
 
@@ -384,8 +312,8 @@ int main(int argc, char** argv){
     auto mplanner = std::make_unique<MotionPlanner>();
     mplanner->init(std::move(space), std::move(heurs[0]), std::move(inad_heurs), planner_params );
 
-    //MotionPlannerROS<Callbacks, ReadExperimentFromFile>(nh) mplanner_ros;
-    //mplanner.setPlannerParams(m_planner_params);
+    MotionPlannerROS< Callbacks, ReadExperimentFromFile, MotionPlanner > mplanner_ros(nh);
+    mplanner_ros.setPlannerParams(planner_params);
 
     while(ros::ok()) {
         loop_rate.sleep();
