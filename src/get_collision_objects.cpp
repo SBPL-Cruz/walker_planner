c8ed8915103c551a8cb7a0a172b204b5f26aa929
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-09-04 18:31:27 -0400

    Use config struct to build multi room map
    Also add tables of randomized sized and at random locations.

--- src/get_collision_objects.cpp
+++ src/get_collision_objects.cpp
@@ -1,4 +1,5 @@
 #include "get_collision_objects.h"
+#include "utils.h"
 
 moveit_msgs::CollisionObject GetCollisionCube(
     const geometry_msgs::Pose& pose,
@@ -110,6 +111,187 @@ std::vector<moveit_msgs::CollisionObject> GetCollisionObjects(
 
 std::vector<moveit_msgs::CollisionObject> GetMultiRoomMapCollisionCubes(
     const std::string& frame_id,
+    const MultiRoomMapConfig config){
+    srand(config.seed);
+    // Wall Thickness
+    const double th = 0.05;
+    // Wall Height
+    const double ht = config.h_max;
+    // Alley Width
+    const double alley_width = config.alley_width;
+
+    const double x_max = config.x_max;
+    const double y_max = config.y_max;
+    const double door_width = config.door_width;
+    const int n_tables = config.n_tables;
+
+    std::string object_id;
+    std::vector<moveit_msgs::CollisionObject> objs;
+    std::vector<double> dims(3,0);
+    geometry_msgs::Pose pose;
+    pose.orientation.x = 0;
+    pose.orientation.y = 0;
+    pose.orientation.z = 0;
+    pose.orientation.w = 1;
+
+    // Description of Map:
+    // Two columns of rooms separated by an alley.
+    // The first column has three equal size rooms vertically arranged.
+    // The bottom room has a door in the center.
+    // The top room has a door at far-right.
+    //
+    // The second column has two equal size rooms vertically arranged.
+    // They have a door joining them at the center of the common wall.
+    //
+    // The alley has a door to access each room except the middle room in the
+    // first column. All these doors are at the center of the wall shared with
+    // the alley.
+
+    // Map
+    double map_half_y = y_max/2;
+    double map_half_x = x_max/2;
+    double map_third_y = y_max/3;
+    double map_third_x = x_max/3;
+    double d = door_width/2;
+    double z = ht/2;
+
+    // Alley
+    double alley_left_wall_x = map_half_x;
+    double alley_right_wall_x = alley_left_wall_x + alley_width;
+
+    // Alley bottom left wall
+    object_id = "wall1";
+    pose.position.x = alley_left_wall_x;
+    pose.position.y = ((map_third_y/2) - d)/2;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = map_third_y/2 - d;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    // Alley bottom right wall
+    object_id = "wall2";
+    pose.position.x = alley_right_wall_x;
+    pose.position.y = (map_half_y/2 - d)/2;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = 2*pose.position.y;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    // First column, bottom room
+    double top_wall_length = map_half_x/2 - d;
+    object_id = "wall3";
+    pose.position.x = top_wall_length/2;
+    pose.position.y = map_third_y;
+    pose.position.z = z;
+    dims[0] = top_wall_length;
+    dims[1] = th;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    object_id = "wall4";
+    pose.position.x = map_half_x - top_wall_length/2;
+    pose.position.y = map_third_y;
+    pose.position.z = z;
+    dims[0] = top_wall_length;
+    dims[1] = th;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    double second_col_bottom_wall_length = map_half_y/2 - d;
+    // Alley
+    object_id = "wall5";
+    pose.position.x = alley_right_wall_x;
+    pose.position.y = map_half_y;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = map_half_y - 2*d;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    object_id = "wall6";
+    pose.position.x = alley_left_wall_x;
+    pose.position.y = map_half_y;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = 2*map_third_y - 2*d;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    // Second Column
+    double common_wall_length = (map_half_x - alley_width)/2 - d;
+    object_id = "wall8";
+    pose.position.x = alley_right_wall_x + common_wall_length/2;
+    pose.position.y = map_half_y;
+    pose.position.z = z;
+    dims[0] = common_wall_length;
+    dims[1] = th;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    object_id = "wall9";
+    pose.position.x = x_max - common_wall_length/2;
+    pose.position.y = map_half_y;
+    pose.position.z = z;
+    dims[0] = common_wall_length;
+    dims[1] = th;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    //Tables
+    //Tables are located in the bottom room, 2nd column.
+    std::vector<std::pair<double, double>> table_locs;
+    for(int i=0; i<n_tables; i++){
+        object_id = "table" + std::to_string(i);
+        bool no_overlap = false;
+        while(!no_overlap){
+            pose.position.x = getRandNum( alley_right_wall_x + config.max_table_len/2, x_max - config.max_table_len/2 );
+            pose.position.y = getRandNum( config.max_table_len/2, map_half_y - config.max_table_len/2 );
+            if(minDistFromEach(std::make_pair(pose.position.x, pose.position.y),
+                        table_locs) > config.min_dist_bw_tables)
+                no_overlap = true;
+        }
+        pose.position.z = config.table_height/2;
+        dims[0] = getRandNum( config.min_table_len, config.max_table_len );
+        dims[1] = getRandNum( config.min_table_len, config.max_table_len );
+        dims[2] = config.table_height;
+        objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+        table_locs.push_back(std::make_pair(pose.position.x, pose.position.y));
+    }
+
+    // First Column
+    object_id = "wall10";
+    pose.position.x = map_half_x/2;
+    pose.position.y = 2*map_third_y;
+    pose.position.z = z;
+    dims[0] = map_half_x - 2*d;
+    dims[1] = th;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    //Second Column
+    object_id ="wall11";
+    pose.position.x = alley_right_wall_x;
+    pose.position.y = y_max - second_col_bottom_wall_length/2;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = second_col_bottom_wall_length;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    // First Column
+    object_id ="wall12";
+    pose.position.x = alley_left_wall_x;
+    pose.position.y = y_max - (map_third_y/2 - d)/2;
+    pose.position.z = z;
+    dims[0] = th;
+    dims[1] = map_third_y/2 - d;
+    dims[2] = ht;
+    objs.push_back(GetCollisionCube(pose, dims, frame_id, object_id));
+
+    return objs;
+}
     const double x_max,
     const double y_max,
     const double door_width,
