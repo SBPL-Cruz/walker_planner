d5b94168eee30f788d4db77fb7eb2decc22ffdc6
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-09-07 02:06:23 -0400

    Disable all callbacks and waiting for them.

--- src/motion_planner_ros.cpp
+++ src/motion_planner_ros.cpp
@@ -17,14 +17,14 @@ Callbacks::Callbacks(ros::NodeHandle _nh,
     ros::param::set("/walker_planner_request", 0);
     ros::param::set("/walker_planner_done", 0);
     m_path_pub = m_nh.advertise<walker_planner::Path1>("Robot_path", 1000);
-    m_sub_occgrid = m_nh.subscribe("/map", 1000, &Callbacks::occgridCallback, this);
-    m_sub_octomap = m_nh.subscribe("/octomap_binary", 1000, &Callbacks::octomapCallback, this);
+    //m_sub_occgrid = m_nh.subscribe("/map", 1000, &Callbacks::occgridCallback, this);
+    //m_sub_octomap = m_nh.subscribe("/octomap_binary", 1000, &Callbacks::octomapCallback, this);
     //m_sub_start = m_nh.subscribe("/poseupdate", 1000, &Callbacks::startCallback, this);
     //m_sub_pose = m_nh.subscribe("/Grasps", 1000, &Callbacks::poseCallback, this);
 
     m_status_variables = { //&m_start_received,
-            &m_octomap_received,
-            &m_occgrid_received
+            //&m_octomap_received,
+            //&m_occgrid_received
             //&m_grasp_received
             };
 }
