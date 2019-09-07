b09640fbf646ab8b6ccbbdf79b964565cc92ccd3
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-09-07 02:03:55 -0400

    Added code to try/test the planner.

---
+++ src/debug_planner.cpp
@@ -0,0 +1,277 @@
+#include <tf2/LinearMath/Quaternion.h>
+#include <sstream>
+#include <memory>
+#include <fstream>
+#include <cmath>
+#include <algorithm>
+
+#include <ros/ros.h>
+#include <smpl/graph/manip_lattice_action_space.h>
+#include <smpl/graph/manip_lattice_multi_rep.h>
+#include <smpl/graph/motion_primitive.h>
+#include <smpl/heuristic/bfs_heuristic.h>
+#include <smpl/heuristic/bfs_fullbody_heuristic.h>
+#include <smpl/heuristic/bfs_base_rot_heuristic.h>
+#include <smpl/heuristic/euclid_dist_heuristic.h>
+#include <smpl/heuristic/euclid_diffdrive_heuristic.h>
+#include <smpl/heuristic/arm_retract_heuristic.h>
+#include <smpl/types.h>
+#include <sbpl/planners/mrmhaplanner.h>
+
+#include "get_collision_objects.h"
+#include "motion_planner.h"
+#include "motion_planner_ros.h"
+
+int main(int argc, char**argv){
+    ros::init(argc, argv, "test_fk");
+    ros::NodeHandle nh;
+    ros::NodeHandle ph("~");
+    ros::Rate loop_rate(10);
+    smpl::VisualizerROS visualizer(nh, 100);
+    smpl::viz::set_visualizer(&visualizer);
+
+    /////////////////
+    // Robot Model //
+    /////////////////
+
+    ROS_INFO("Load common parameters");
+    auto robot_description_key = "robot_description";
+    std::string robot_description_param;
+    if (!nh.searchParam(robot_description_key, robot_description_param)) {
+        ROS_ERROR("Failed to find 'robot_description' key on the param server");
+        return 1;
+    }
+
+    std::string robot_description;
+    if (!nh.getParam(robot_description_param, robot_description)) {
+        ROS_ERROR("Failed to retrieve param 'robot_description' from the param server");
+        return 1;
+    }
+
+    // Reads planning_joints, frames.
+    RobotModelConfig robot_config;
+    if (!ReadRobotModelConfig(ros::NodeHandle("~robot_model"), robot_config)) {
+        ROS_ERROR("Failed to read robot model config from param server");
+        return 1;
+    }
+
+    std::string planning_frame;
+    if (!ph.getParam("planning_frame", planning_frame)) {
+        ROS_ERROR("Failed to retrieve param 'planning_frame' from the param server");
+        return 1;
+    }
+
+    ////////////////////
+    // Occupancy Grid //
+    ////////////////////
+
+    ROS_INFO("Initialize Occupancy Grid");
+
+    auto df_size_x = 20.0;
+    auto df_size_y = 15.0;
+    auto df_size_z = 1.5;
+    auto df_res = 0.05;
+    auto df_origin_x = 0;
+    auto df_origin_y = 0;
+    auto df_origin_z = 0.0;
+    auto max_distance = 1.0;
+
+    using DistanceMapType = smpl::EuclidDistanceMap;
+
+    auto df = std::make_shared<DistanceMapType>(
+            df_origin_x, df_origin_y, df_origin_z,
+            df_size_x, df_size_y, df_size_z,
+            df_res,
+            max_distance);
+
+    auto ref_counted = false;
+    auto grid_ptr = std::make_unique<smpl::OccupancyGrid>(df, ref_counted);
+
+    grid_ptr->setReferenceFrame(planning_frame);
+    SV_SHOW_INFO(grid_ptr->getBoundingBoxVisualization());
+
+    //////////////////////////////////
+    // Initialize Collision Checker //
+    //////////////////////////////////
+
+    ROS_INFO("Initialize collision checker");
+
+    // This whole manage storage for all the scene objects and must outlive
+    // its associated CollisionSpace instance.
+    //auto scene_ptr = std::make_unique<CollisionSpaceScene>();
+    auto scene_ptr = std::make_unique<CollisionSpaceScene>();
+
+    smpl::collision::CollisionModelConfig cc_conf;
+    if (!smpl::collision::CollisionModelConfig::Load(ph, cc_conf)) {
+        ROS_ERROR("Failed to load Collision Model Config");
+        return 1;
+    }
+
+    ROS_INFO("collision model loaded");
+    smpl::collision::CollisionSpace cc;
+    if (!cc.init(
+            grid_ptr.get(),
+            robot_description,
+            cc_conf,
+            robot_config.group_name,
+            robot_config.planning_joints))
+    {
+        ROS_ERROR("Failed to initialize Collision Space");
+        return 1;
+    }
+    cc.setPadding(0.02);
+    cc.setWorldToModelTransform(Eigen::Affine3d::Identity());
+
+    //auto marker = cc.getCollisionRobotVisualization(start_state.joint_state.position);
+    //marker[0].ns = "Start state";
+    //SV_SHOW_INFO(marker);
+
+    /////////////////
+    // Setup Scene //
+    /////////////////
+
+    PlannerConfig planning_config;
+    if (!ReadPlannerConfig(ros::NodeHandle("~planning"), planning_config)){
+        ROS_ERROR("Failed to read planner config");
+        return 1;
+    }
+    planning_config.cost_per_cell = 1000;
+    ROS_INFO("Initialize scene");
+
+    scene_ptr->SetCollisionSpace(&cc);
+
+    ROS_INFO("Setting up robot model");
+    auto rm = SetupRobotModel(robot_description, robot_config);
+    if (!rm) {
+        ROS_ERROR("Failed to set up Robot Model");
+        return 1;
+    }
+
+    auto map_config = getMultiRoomMapConfig(ph);
+    auto objects = GetMultiRoomMapCollisionCubes(grid_ptr->getReferenceFrame(), map_config);
+    for(auto object : objects)
+        scene_ptr->ProcessCollisionObjectMsg(object);
+
+    rm->printRobotModelInformation();
+
+    SV_SHOW_INFO(grid_ptr->getDistanceFieldVisualization(1.0));
+
+    SV_SHOW_INFO(scene_ptr->getOccupiedVoxelsVisualization());
+    SV_SHOW_INFO(cc.getCollisionRobotVisualization());
+    SV_SHOW_INFO(cc.getCollisionWorldVisualization());
+
+    ros::Duration(1.0).sleep();
+
+    //for(int i=0; i<100; i++)
+        //ROS_ERROR("Distance: %f", grid_ptr->getDistance(i, i, 10));
+
+    auto resolutions = getResolutions( rm.get(), planning_config );
+    auto action_space = std::make_unique<smpl::ManipLatticeActionSpace>();
+    auto space = std::make_unique<smpl::ManipLattice>();
+
+    if (!space->init( rm.get(), &cc, resolutions, action_space.get() )) {
+        SMPL_ERROR("Failed to initialize Manip Lattice");
+        return 1;
+    }
+
+    if (!action_space->init(space.get())) {
+        SMPL_ERROR("Failed to initialize Manip Lattice Action Space");
+        return 1;
+    }
+
+    if(!action_space->load(planning_config.mprim_filename))
+        return 1;
+
+    space->setVisualizationFrameId(grid_ptr->getReferenceFrame());
+
+    using MotionPrimitive = smpl::MotionPrimitive;
+    action_space->useMultipleIkSolutions(planning_config.use_multiple_ik_solutions);
+    action_space->useAmp(MotionPrimitive::SNAP_TO_XYZ, planning_config.use_xyz_snap_mprim);
+    action_space->useAmp(MotionPrimitive::SNAP_TO_RPY, planning_config.use_rpy_snap_mprim);
+    action_space->useAmp(MotionPrimitive::SNAP_TO_XYZ_RPY, planning_config.use_xyzrpy_snap_mprim);
+    action_space->useAmp(MotionPrimitive::SHORT_DISTANCE, planning_config.use_short_dist_mprims);
+    action_space->ampThresh(MotionPrimitive::SNAP_TO_XYZ, planning_config.xyz_snap_dist_thresh);
+    action_space->ampThresh(MotionPrimitive::SNAP_TO_RPY, planning_config.rpy_snap_dist_thresh);
+    action_space->ampThresh(MotionPrimitive::SNAP_TO_XYZ_RPY, planning_config.xyzrpy_snap_dist_thresh);
+    action_space->ampThresh(MotionPrimitive::SHORT_DISTANCE, planning_config.short_dist_mprims_thresh);
+
+    moveit_msgs::RobotState start_state;
+    if (!ReadInitialConfiguration(ph, start_state)) {
+        ROS_ERROR("Failed to get initial configuration.");
+    }
+
+    smpl::RobotState initial_positions;
+    std::vector<std::string> missing;
+    if (!leatherman::getJointPositions(
+            start_state.joint_state,
+            start_state.multi_dof_joint_state,
+            rm->getPlanningJoints(),
+            initial_positions,
+            missing)){
+        ROS_WARN_STREAM("start state is missing planning joints: " << missing);
+
+        moveit_msgs::RobotState fixed_state = start_state;
+        for (auto& variable : missing) {
+            ROS_WARN("  Assume position 0.0 for joint variable '%s'", variable.c_str());
+            fixed_state.joint_state.name.push_back(variable);
+            fixed_state.joint_state.position.push_back(0.0);
+        }
+
+        if (!leatherman::getJointPositions(
+                fixed_state.joint_state,
+                fixed_state.multi_dof_joint_state,
+                rm->getPlanningJoints(),
+                initial_positions,
+                missing)){
+            return 1;
+        }
+    }
+
+    smpl::Affine3 goal_pose;
+
+    std::vector<double> goal_state( 6, 0 );
+    ph.param("goal/x", goal_state[0], 0.0);
+    ph.param("goal/y", goal_state[1], 0.0);
+    ph.param("goal/z", goal_state[2], 0.0);
+    ph.param("goal/roll", goal_state[3], 0.0);
+    ph.param("goal/pitch", goal_state[4], 0.0);
+    ph.param("goal/yaw", goal_state[5], 0.0);
+
+    geometry_msgs::Pose read_grasp;
+    read_grasp.position.x = goal_state[0];
+    read_grasp.position.y = goal_state[1];
+    read_grasp.position.z = goal_state[2];
+    tf::Quaternion q;
+    q.setRPY( goal_state[3], goal_state[4], goal_state[5] );
+    read_grasp.orientation.x = q[0];
+    read_grasp.orientation.y = q[1];
+    read_grasp.orientation.z = q[2];
+    read_grasp.orientation.w = q[3];
+    tf::poseMsgToEigen( read_grasp, goal_pose);
+
+    smpl::GoalConstraint goal;
+    goal.type = smpl::GoalType::XYZ_RPY_GOAL;
+    goal.pose = goal_pose;
+    goal.xyz_tolerance[0] = 0.03;
+    goal.xyz_tolerance[1] = 0.03;
+    goal.xyz_tolerance[2] = 0.03;
+    goal.rpy_tolerance[0] = 0.30;
+    goal.rpy_tolerance[1] = 0.30;
+    goal.rpy_tolerance[2] = 0.30;
+
+    auto bfs_3d_base_heur = std::make_unique<smpl::Bfs3DBaseHeuristic>();
+    if(!bfs_3d_base_heur->init(space.get(), grid_ptr.get(), 16)){
+        ROS_ERROR("Could not init bfs heuristic.");
+        return 1;
+    }
+
+    space->setStart(initial_positions);
+    space->setGoal(goal);
+    bfs_3d_base_heur->updateGoal(goal);
+
+    auto h_val = bfs_3d_base_heur->GetGoalHeuristic(space->getStartStateID());
+    ROS_ERROR("Heuristic: %d", h_val);
+
+
+    return 0;
+}
