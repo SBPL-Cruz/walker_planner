5247e997b42bd5ab7fececea8019d2c7e0a23318
Aditya Agarwal <aditya.kgp25@gmail.com>
2019-11-16 19:39:19 -0500

    added encoder and decoder

--- cvae/model.py
+++ cvae/model.py
@@ -10,11 +10,78 @@ from torch.utils.data import DataLoader
 from model_constants import *
 
 
+class Encoder(nn.Module):
+
+    def __init__(self, layer_sizes, latent_dim, conditional, num_labels):
+
+        super().__init__()
+
+        self.conditional = conditional
+        if self.conditional:
+            layer_sizes[0] += num_labels
+
+        self.MLP = nn.Sequential()
+
+        for i, (in_size, out_size) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+            self.MLP.add_module(
+                name="L{:d}".format(i), module=nn.Linear(in_size, out_size))
+            self.MLP.add_module(name="A{:d}".format(i), module=nn.ReLU())
+
+        self.linear_means = nn.Linear(layer_sizes[-1], latent_dim)
+        self.linear_log_var = nn.Linear(layer_sizes[-1], latent_dim)
+
+    def forward(self, x, c=None):
+
+        if self.conditional:
+            c = idx2onehot(c, n=10)
+            x = torch.cat((x, c), dim=-1)
+
+        x = self.MLP(x)
+
+        means = self.linear_means(x)
+        log_vars = self.linear_log_var(x)
+
+        return means, log_vars
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, layer_sizes, latent_dim, conditional, num_labels):
+
+        super().__init__()
+
+        self.MLP = nn.Sequential()
+
+        self.conditional = conditional
+        if self.conditional:
+            input_size = latent_dim + num_labels
+        else:
+            input_size = latent_dim
+
+        for i, (in_size, out_size) in enumerate(zip([input_size]+layer_sizes[:-1], layer_sizes)):
+            self.MLP.add_module(
+                name="L{:d}".format(i), module=nn.Linear(in_size, out_size))
+            if i+1 < len(layer_sizes):
+                self.MLP.add_module(name="A{:d}".format(i), module=nn.ReLU())
+            else:
+                self.MLP.add_module(name="sigmoid", module=nn.Sigmoid())
+
+    def forward(self, z, c):
+
+        if self.conditional:
+            c = idx2onehot(c, n=10)
+            z = torch.cat((z, c), dim=-1)
+
+        x = self.MLP(z)
+
+        return x
+
 class CVAE(nn.Module):
 
     def __init__(self,
                  x_dmin=X_DIM,
                  c_dmin=C_DIM,
+                 latent_dim=LATENT_DIM,
                  run_id=1,
                  experiment_path_prefix=EXPERIMENT_PATH_PREFIX,
                  hiddens=HIDDEN_LAYERS,
@@ -25,24 +92,30 @@ class CVAE(nn.Module):
                  use_tboard=USE_TENSORBOARD,
                  tf_sub_path=TF_PATH):
         """
-        :param num_nets: number of networks in the ensemble
-        :param x_dim: state dimension
-        :param c_dim: action dimension
+        :param x_dim: x dimension
+        :param c_dim: c dimension
         :param learning_rate:
         """
         super(CVAE, self).__init__()
-        # self.sess = tf.Session()
         self.x_dim = x_dmin
         self.c_dim = c_dmin
+        self.latent_dim = latent_dim
 
         self.cuda_available = cuda_available
         self.device = torch.device('cuda' if CUDA_AVAILABLE else 'cpu')
-        # K.set_session(self.sess)
 
 
         self.hiddens = hiddens
 
         self.model = self.create_network()
+
+        self.encoder = Encoder(
+            hiddens, self.latent_dim, True, self.c_dim)
+        self.decoder = Decoder(
+            hiddens, self.latent_dim, True, self.c_dim)
+
+        print("Encoder : {}".format(self.encoder))
+        print("Decoder : {}".format(self.decoder))
         self.initial_learning_rate = initial_learning_rate
         self.current_lr = self.initial_learning_rate
         self.weight_decay = weight_decay
