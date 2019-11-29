6dadf992f0e692ad309ddb89f933f05a7e10282b
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-11-28 20:01:17 -0500

    Implemented function to construct RTree by reading (x,y) points from file.

--- include/cvae_scheduling_policies.h
+++ include/cvae_scheduling_policies.h
@@ -2,7 +2,15 @@
 #define CVAE_SCHEDULING_POLICIES_H
 
 #include <vector>
+#include <utility>
+#include <fstream>
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/index/rtree.hpp>
 #include <sbpl/planners/scheduling_policy.h>
+
+namespace bg = boost::geometry;
+namespace bgi = boost::geometry::index;
 
 /** Base Class for Policies making use of CVAE**/
 class CVAEPolicy : public SchedulingPolicy
@@ -32,9 +40,15 @@ class CVAENNPolicy : public CVAEPolicy
     // Reads (x, y) points in a map along with their rep label.
     // Constructs n k-d trees with those points- one for each rep.
     // Constructing different k-d trees is makes query faster (O(log n)).
-    int loadRepDistribution(std::string file_name);
+    bool loadRepDistribution(std::string file_name);
 
     private:
+    using Point = bg::model::point<double, 2, bg::cs::cartesian>;
+    using Value = std::pair<Point, unsigned int>;
+    // Stores base (x, y) points where are representation should be searched.
+    // Linear has fastest construction time but slowest query time.
+    // rstar has slowest construction time but fastest query time.
+    bgi::rtree< Value, bgi::rstar<16> > m_kd_tree_arm;
 };
 
 #endif
--- src/cvae_scheduling_policies.cpp
+++ src/cvae_scheduling_policies.cpp
@@ -1,4 +1,5 @@
-#include "cvae_scheduling_policy.h"
+#include <ros/ros.h>
+#include "cvae_scheduling_policies.h"
 
 CVAEPolicy::CVAEPolicy( int _num_arms ) :
     SchedulingPolicy(_num_arms)
@@ -18,4 +19,31 @@ int CVAENNPolicy::getAction(int _state_id)
 {
     int action = 0;
     return action;
+}
+
+bool CVAENNPolicy::loadRepDistribution(std::string _file_name)
+{
+    std::ifstream ifs;
+    ifs.open(_file_name, std::ios::in);
+    if(!ifs.is_open())
+    {
+        ROS_ERROR("Could not open Distribution file.");
+        return false;
+    }
+
+    std::string line;
+    unsigned int idx = 0;
+    while(ifs >> line)
+    {
+        double x, y;
+        char comma;
+        std::istringstream ss(line);
+        ss >> x >> comma >> y;
+        Point point = Point(x, y);
+        m_kd_tree_arm.insert(std::make_pair(point, idx));
+        idx++;
+    }
+
+    ifs.close();
+    return true;
 }
